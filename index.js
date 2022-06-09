89ccfc94e1d02a2cb4008d0019936f58dff2b1c5
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-09 05:05:40 -0700

    commit tree 297f5aa62efd3c7af49a43d54a64ea39a355c26d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(ops="*") {
+    return function(name="special") {
+      return `You are ${ops}${name}${ops}!`
+    }
+  }
+
+
+
